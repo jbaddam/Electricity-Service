95410903c0ca2e1b54b8a791830f7b7d28e7c170
jaganreddy0927 <jaganreddy0927@gmail.com>
2016-08-16 16:31:06 -0500

    data source updated

--- Registration/src/main/java/com/bootcampjava/electricityservice/daos/registration/RegistrationDAO.java
+++ Registration/src/main/java/com/bootcampjava/electricityservice/daos/registration/RegistrationDAO.java
@@ -16,7 +16,8 @@ public class RegistrationDAO{
 	@Autowired
 	private SessionFactory mySessionFactory;
 
-	public int saveCustomer(Customer customer) {	
+	public int saveCustomer(Customer customer) {
+		
 		Session session = mySessionFactory.getCurrentSession();	
 		Transaction trans = session.beginTransaction();
 		int custId = (Integer) session.save(customer);	
